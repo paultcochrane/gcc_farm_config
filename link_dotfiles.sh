84dc8040cbe4acb90a45e636f7180b8561562625
Paul Cochrane <paul@liekut.de>
2014-11-16 20:32:22 +0100

    Adding a script to link the relevant dotfiles

---
+++ link_dotfiles.sh
@@ -0,0 +1,5 @@
+ #!/usr/bin/bash
+
+ln -s $HOME/p5smoke/gcc_farm_config/.gitconfig $HOME/.gitconfig
+ln -sf $HOME/p5smoke/gcc_farm_config/.bashrc $HOME/.bashrc
+ln -s $HOME/p5smoke/gcc_farm_config/.vimrc $HOME/.vimrc
