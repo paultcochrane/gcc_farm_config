c40d6797f0e666202e750e422499d958ef4ef2b2
Paul Cochrane <paul@liekut.de>
2014-11-16 22:08:14 +0100

    Adding a script to copy config files to git branch

---
+++ copy_config_files_to_git_branch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+FILES="perlcurrent.cfg perlcurrent.cfg.bak smokecurrent_config \
+       smokecurrent.patchup smokecurrent.sh smokecurrent.skiptests \
+       smokecurrent.usernote"
+for file in $FILES
+do
+    cp $HOME/p5smoke/smoke/$file $HOME/p5smoke/gcc_farm_config/$file
+done
